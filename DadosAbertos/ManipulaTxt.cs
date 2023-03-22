101b0161758244e3c5b7259b346243b7d473bd43
JaoBrito <jvbrito02@gmail.com>
2023-03-22 12:10:24 -0300

    😁

---
+++ DadosAbertos/ManipulaTxt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+var path = @"C:\Users\joao.brito\Desktop\original.EMPRECSV";
+var newPath = @"C:\Users\joao.brito\Desktop\DadosFormatados\editado";
+var contador = 1;
+
+try
+{
+    using (StreamReader sr = new StreamReader(path))
+    {
+        StreamWriter sw = new StreamWriter(newPath + contador.ToString() + ".csv");
+        for (int count = 0; count < 100; count++)
+        {
+            sw.WriteLine("cnpj_basico;razao_social;natureza_jur;qual_resp;capital_social;porta_empresa;ent_fed");
+            for (int i = 0; i < 10000; i++)
+            {
+                var texto = sr.ReadLine();
+                sw.WriteLine(texto);
+            }
+        sw.Close();
+        contador++;
+        sw = new StreamWriter(newPath + contador.ToString() + ".csv");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+// ⬇ Nesse formato com AppendText o programa fica mais lento
+//copia uma linha, fecha o arquivo, abre outra e repete o processo
+
+//try {
+//    using (StreamReader sr = new StreamReader(path)) {
+//        for (int count = 0; count < 100; count++) {
+//            for (int i = 0; i < 10000; i++) {
+//                var texto = sr.ReadLine();
+//                using (StreamWriter sw = File.AppendText(newPath)) {
+//                    sw.WriteLine(texto);
+//                }
+//            }
+//        }
+//    }
+//} catch (Exception ex) {
+//    Console.WriteLine(ex.Message);
+//}
