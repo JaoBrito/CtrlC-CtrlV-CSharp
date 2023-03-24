3359f95a4a8a89807986692fec5fe52f36a64c85
JaoBrito <jvbrito02@gmail.com>
2023-03-24 11:04:09 -0300

    finished project

--- DadosAbertos/ManipulaTxt.cs
+++ DadosAbertos/ManipulaTxt.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
 
-var path = @"C:\Users\user\Desktop\original.EMPRECSV";
-var newPath = @"C:\Users\user\Desktop\DadosFormatados\editado";
+// Mudar o user -> joao.brito
+var path = @"C:\Users\joao.brito\Desktop\socios4.SOCIOCSV";
+var newPath = @"C:\Users\joao.brito\Desktop\DadosFormatados\socios4\editado";
 var contador = 1;
 
 try
@@ -10,28 +11,31 @@ try
     using (StreamReader sr = new StreamReader(path))
     {
         StreamWriter sw = new StreamWriter(newPath + contador.ToString() + ".csv");
-        for (int count = 0; count < 13; count++)
+        for (int count = 0; count < 3; count++)
         {
-             if(count != 0){
-             sw.WriteLine("cnpj_basico;razao_social;natureza_jur;qual_resp;capital_social;porta_empresa;ent_fed");
-            for (int i = 0; i < 1000000; i++)
-            {
-                var texto = sr.ReadLine();
-                sw.WriteLine(texto);
-            }
-        sw.Close();
-        contador++;
-        sw = new StreamWriter(newPath + contador.ToString() + ".csv");   
-           } else {
-                for (int i = 0; i < 1000000; i++)
-            {
-                var texto = sr.ReadLine();
-                sw.WriteLine(texto);
-            }
-        sw.Close();
-        contador++;
-        sw = new StreamWriter(newPath + contador.ToString() + ".csv");
-            }
+            // if (count != 0) Para arquivos que já venham com o cabeçalho
+            // {
+                sw.WriteLine("cnpj;tipo_socio;nome_socio;cnpj_cpf_socio;cod_qualificacao;data_entrada;cod_pais_ext;cpf_repres;nome_repres;cod_qualif_repres;faixa_etaria");
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    var texto = sr.ReadLine();
+                    sw.WriteLine(texto);
+                }
+                sw.Close();
+                contador++;
+                sw = new StreamWriter(newPath + contador.ToString() + ".csv");
+            // }
+            // else
+            // {
+            //     for (int i = 0; i < 1_000_000; i++)
+            //     {
+            //         var texto = sr.ReadLine();
+            //         sw.WriteLine(texto);
+            //     }
+            //     sw.Close();
+            //     contador++;
+            //     sw = new StreamWriter(newPath + contador.ToString() + ".csv");
+            // }
         }
     }
 }
@@ -39,8 +43,6 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
-
 
 // ⬇ Nesse formato com AppendText o programa fica mais lento
 //copia uma linha, fecha o arquivo, abre outra e repete o processo
