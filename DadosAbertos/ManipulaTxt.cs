bb74aeec6a26575b1900af5decd50aff5fd94a96
JaoBrito <jvbrito02@gmail.com>
2023-04-03 15:04:16 -0300

    👍

--- DadosAbertos/ManipulaTxt.cs
+++ DadosAbertos/ManipulaTxt.cs
@@ -2,8 +2,8 @@
 using System.IO;
 
 // Mudar o user -> joao.brito
-var path = @"C:\Users\joao.brito\Desktop\socios4.SOCIOCSV";
-var newPath = @"C:\Users\joao.brito\Desktop\DadosFormatados\socios4\editado";
+var path = @"C:\Users\joao.brito\Desktop\estabelecimento9.CSV";
+var newPath = @"C:\Users\joao.brito\Desktop\DadosFormatados\estabelecimento9\editado";
 var contador = 1;
 
 try
@@ -11,11 +11,12 @@ try
     using (StreamReader sr = new StreamReader(path))
     {
         StreamWriter sw = new StreamWriter(newPath + contador.ToString() + ".csv");
-        for (int count = 0; count < 3; count++)
-        {
-            // if (count != 0) Para arquivos que já venham com o cabeçalho
-            // {
-                sw.WriteLine("cnpj;tipo_socio;nome_socio;cnpj_cpf_socio;cod_qualificacao;data_entrada;cod_pais_ext;cpf_repres;nome_repres;cod_qualif_repres;faixa_etaria");
+        
+            for (int count = 0; count < 5; count++)
+            {
+                // if (count != 0) Para arquivos que já venham com o cabeçalho
+                // {
+                sw.WriteLine("cnpj_basico;cnpj_ordem;cnpj_dv;id_matriz_filial;nome_fantasia;situacao_cadastral;data_situacao;motivo_situacao;cidade_ex;pais;inicio_atividade;cnae_principal;cnae_secundaria;logradouro;numero;complemento;bairro;cep;uf;municipio;;ddd1;tel1;ddd2;tel2;ddd_fax;fax;email;situacao_especial;data_especial");
                 for (int i = 0; i < 1_000_000; i++)
                 {
                     var texto = sr.ReadLine();
@@ -24,19 +25,20 @@ try
                 sw.Close();
                 contador++;
                 sw = new StreamWriter(newPath + contador.ToString() + ".csv");
-            // }
-            // else
-            // {
-            //     for (int i = 0; i < 1_000_000; i++)
-            //     {
-            //         var texto = sr.ReadLine();
-            //         sw.WriteLine(texto);
-            //     }
-            //     sw.Close();
-            //     contador++;
-            //     sw = new StreamWriter(newPath + contador.ToString() + ".csv");
-            // }
-        }
+                // }
+                // else
+                // {
+                //     for (int i = 0; i < 1_000_000; i++)
+                //     {
+                //         var texto = sr.ReadLine();
+                //         sw.WriteLine(texto);
+                //     }
+                //     sw.Close();
+                //     contador++;
+                //     sw = new StreamWriter(newPath + contador.ToString() + ".csv");
+                // }
+            }
+
     }
 }
 catch (Exception ex)
